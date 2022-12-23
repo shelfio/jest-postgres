33b33c32a1b7a2ee778b692f7a2742c385db2422
harazdovskiy <harazdovskiy@gmail.com>
2022-12-23 13:36:43 +0200

    fix: fixed teardown

--- src/teardown.ts
+++ src/teardown.ts
@@ -1,5 +1,7 @@
 import {stop} from '@shelf/postgres-local';
+import cwd from 'cwd';
 
 module.exports = async function stopPostgres() {
-  await stop();
+  const config = require(`${cwd()}/jest-postgres-config.js`);
+  await stop(config?.version);
 };
